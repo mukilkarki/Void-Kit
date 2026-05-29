54b3a8beb86b8ef2ebfbdcc47438a47577ee69d7
MUKIL KARKI M <mukilkarkimail@gmail.com>
2026-05-29 19:43:01 +0530

    del all

--- server.py
+++
@@ -1,5 +0,0 @@
-# Deprecated Python server implementation.
-# This file is no longer used in the Render deployment.
-# The application now runs using the Node.js server defined in server.js,
-# which manages ngrok via the npm ngrok package.
-# You may safely delete this file in the future.
